81ad41c569ca49536b21d4d2813fc76dbb1e3dc0
brandonb95 <brandonbirk1995@gmail.com>
2023-02-02 22:51:05 -0800

    WIP of displaying overview

--- movie-database/src/components/Movies/Card.js
+++ movie-database/src/components/Movies/Card.js
@@ -16,6 +16,7 @@ function Card({
         <div>
             <div className="card-poster">
                 <img src={"https://image.tmdb.org/t/p/w500" + posterPath} alt="Movie" />
+                <div className="overview">{overview}</div>
                 <div className="card-title">{title}</div>
                 <div className="release-date">{releaseDate}</div>
                 <div className="average-score">{voteAverage}</div>
