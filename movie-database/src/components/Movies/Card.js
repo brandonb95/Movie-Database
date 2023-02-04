c5a474a32eca50f703842c40686d018fc37c751a
brandonb95 <brandonbirk1995@gmail.com>
2023-02-03 16:35:30 -0800

    Implementing responsive overview length (WIP)

--- movie-database/src/components/Movies/Card.js
+++ movie-database/src/components/Movies/Card.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import  Unselected  from '../../scss/Favourite-Unselected.svg';
 
 
@@ -14,20 +15,45 @@ function Card({
     
 }) {
 
+const [oView, oUpdate] = useState(0);
+
+
+// Change the length of movie overview
+useEffect(() => {
+
+    const resizeWin = () => {
+    const changeOverview = window.innerWidth > 768 ? 100 : 10;
+    
+    oUpdate(changeOverview);
+
+    let value = 0;
+
+    if(window.innerWidth > 1200){
+        value=2;
+    } else if(window.innerWidth > 1600){
+        value=6;
+    }
+
+    oUpdate(value);
+}
+    window.addEventListener("resize", resizeWin);
+},[])
+    
+
+console.log(oView);
+    
+
+
     return (
         <div>
             <div className="card-poster">
+            <p><div className="overview">{overview.substring(0, oView)}...</div></p>
                 <img className="single-poster" src={"https://image.tmdb.org/t/p/w500" + posterPath} alt="Movie" />
-                <img className="movie-favourite" src={Unselected} alt="Unfavourite" />
-
-                <div className="overview">{overview}</div>
-                <div className="card-title">{title}</div>
-                <div className="release-date">{releaseDate}</div>
+                <button className="movie-favourite"></button>
                 <div className="average-score">{voteAverage}</div>
-
-
             </div>
-
+            <div className="card-title">{title}</div>
+            <div className="release-date">{releaseDate}</div>
         </div>
     );
 }
