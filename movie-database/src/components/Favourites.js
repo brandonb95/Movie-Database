d7bb4053aac489c1afb22b5ea30c4c238ec069a5
anhmaile <118566182+anhmaile@users.noreply.github.com>
2023-02-04 22:16:16 -0800

    favourite styling

--- movie-database/src/components/Favourites.js
+++ movie-database/src/components/Favourites.js
@@ -4,10 +4,11 @@ const PageFaves = () => {
     return (
         <body>
             <div className="fav-wrapper">
-                <h1>Your Favourites</h1>
-                <p>Heres a list of your favourite movies & shows!</p>
-                
+            <h1>Your Favourites</h1>
+            <p>Heres a list of your favourite movies & shows!</p>
+            
                 <section className='faves-page'>
+
                     <FavesContainer />
                 </section>
             </div>
